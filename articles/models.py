74ff4d28883adda68193c7c8b3419088914a5bb0
tcseley <tiborcseley@gmail.com>
2021-07-02 11:36:54 -0500

    second commit

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.urls import reverse
+from django.conf import settings
+from django.contrib.auth import get_user_model
+
+
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('article_detail', args=[str(self.id)])
--- config/urls.py
+++ config/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('pages/', include('pages.urls')),
-    
+    path('articles/', include('articles.urls')),
+
 ]
