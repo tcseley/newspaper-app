e173d148a51e06746d6a546bd101780b27ceb4a8
tcseley <tiborcseley@gmail.com>
2021-07-03 02:25:46 -0500

    nightly build

--- articles/models.py
+++ articles/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.urls import reverse
-from django.conf import settings
-from django.contrib.auth import get_user_model
+from django.conf import settings  # we can use -> models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+from django.contrib.auth import get_user_model # <- or this?
 
 
 
@@ -9,7 +9,7 @@ from django.contrib.auth import get_user_model
 class Article(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
-    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- articles/views.py
+++ articles/views.py
@@ -1,38 +1,52 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
-from .models import Article
 from django.urls import reverse_lazy
+from django.contrib.auth import get_user_model
 
+from .models import Article
 # Create your views here.
 
-class ArticleListView(ListView):
+author = get_user_model()
+
+class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
     template_name = 'article_list.html'
 
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin, DetailView):
     model = Article
     template_name = 'article_detail.html'
 
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Article
     fields = ('title', 'body',)
     template_name = 'article_edit.html'
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class ArticleDeleteView(DeleteView):
+
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class ArticleCreateView(CreateView):
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
     template_name = 'article_new.html'
     fields = ('title', 'body',)
-
-    def from_vcalid(self,form):
+    # login_url = '/login/'
+    # redirect_field_name = 'redirect_to'
+    
+    # See line 183 of Django source code django/docs/topics/class-based-views/generic-editing.txt: articles is auto set to signed in user
+    def from_valid(self,form): 
         form.instance.author = self.request.user
         return super().form_valid(form)
