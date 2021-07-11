d49fbf0d94e33c6667c7b3f125d079d971f65152
tcseley <tiborcseley@gmail.com>
2021-07-11 10:08:59 -0500

    modifed weather: view

--- weather/views.py
+++ weather/views.py
@@ -1,13 +1,17 @@
 import json
 import urllib.request
 from django.shortcuts import render
+from environs import Env
+
+env = Env()
+env.read_env()
 
 # Create your views here.
 
 def weather(request):
     if request.method == 'POST':
         city = request.POST['city']
-        source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=b58d40184b28d64493b2d070660c8560').read()
+        source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=' + env.str("API_KEY")).read()
         list_of_data = json.loads(source)
         data = {
             "country_code": str(list_of_data['sys']['country']),
